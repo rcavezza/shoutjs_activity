abc6b4e90f6445a1d3160cbe5f7e99532febb6d2
n0310454 <robert.cavezza@libertymutual.com>
2017-08-18 11:51:52 -0400

    Adds express to serverjs file and creates a dist folder

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('server.js called');
+const express = require('express');
+const path = require('path');
+
+/* eslint-disable no-console */
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, (err) => {
+        if (err) {
+                logger.log('error', err);
+        }
+});
