acad163525ba7b04c686bab3849c6a4f1f599cfe
John Crawford <john.crawford01@libertymutual.com>
2017-08-18 11:58:06 -0400

    added Person class

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -3,13 +3,13 @@ Feature: Hear Shout
   Shouts have a range of approximately 1000m
 
   Scenario: In range shout is heard
-    Given Lucy is at 0, 0
-    And Sean is at 0, 900
+    Given Lucy is at 0, 0 with a volume of 1000
+    And Sean is at 0, 900 with a volume of 1000
     When Sean shouts
     Then Lucy should hear Sean
 
   Scenario: Out of range shout is not heard
-    Given Lucy is at 0, 0
-    And Sean is at 0, 1100
+    Given Lucy is at 0, 0 with a volume of 1000
+    And Sean is at 0, 1100 with a volume of 1000
     When Sean shouts
     Then Lucy should hear nothing
--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const Shouty = require('../../lib/shouty')
-const Coordinate = require('../../lib/coordinate')
+const Person = require('../../lib/person')
 
 const {defineSupportCode} = require('cucumber')
 
@@ -9,18 +9,16 @@ defineSupportCode(({Before, Given, When, Then}) => {
   let shouty
   Before(function() {
     shouty = new Shouty()
+    people = new Map()
   })
 
-  Given('Lucy is at {int}, {int}', function (x, y) {
-    shouty.setLocation('Lucy', new Coordinate(x, y))
+  Given('{str} is at {int}, {int} with a volume of {int}', function (name, x, y, vol) {
+    people.set(name,new Person(name,vol,x,y))
+    shouty.setPerson(people.get(name))
   })
 
-  Given('Sean is at {int}, {int}', function (x, y) {
-    shouty.setLocation('Sean', new Coordinate(x, y))
-  })
-
-  When('Sean shouts', function () {
-    shouty.shout('Sean', ARBITARY_MESSAGE)
+  When('{str} shouts', function (name) {
+    shouty.shout(people.get(name), ARBITARY_MESSAGE)
   })
 
   Then('Lucy should hear Sean', function () {
@@ -30,5 +28,4 @@ defineSupportCode(({Before, Given, When, Then}) => {
   Then('Lucy should hear nothing', function () {
     assert.equal(shouty.getMessagesHeardBy('Lucy').size, 0)
   })
-
 })
